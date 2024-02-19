17561d028d7cf292e7c254185adb9651955bdca7
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-02-19 18:36:00 +0100

    refactoring some header code logic and created the event model class

---
+++ app/model/event.php
@@ -0,0 +1,83 @@
+<?php
+namespace model;
+
+class Event implements \JsonSerializable
+{
+    private int $event_id;
+    private string $name;
+    private string $date;
+    private string $location;
+    private int $price;
+    private string $time;
+    private string $picture;
+
+    // JsonSerialize method
+    public function jsonSerialize(): mixed {
+        return [
+            'event_id' => $this->getEventId(),
+            'name' => $this->getName(),
+            'date' => $this->getDate(),
+            'location' => $this->getLocation(),
+            'price' => $this->getPrice(),
+            'time' => $this->getTime(),
+            'picture' => $this->getPicture(),
+        ];
+    }
+
+    public function getEventId(): int {
+        return $this->event_id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getDate(): string {
+        return $this->date;
+    }
+
+    public function getLocation(): string {
+        return $this->location;
+    }
+
+    public function getPrice(): int {
+        return $this->price;
+    }
+
+    public function getTime(): string {
+        return $this->time;
+    }
+
+    public function getPicture(): string {
+        return $this->picture;
+    }
+
+
+    public function setEventId(int $event_id): void {
+        $this->event_id = $event_id;
+    }
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+
+    public function setDate(string $date): void {
+        $this->date = $date;
+    }
+
+    public function setLocation(string $location): void {
+        $this->location = $location;
+    }
+
+    public function setPrice(int $price): void {
+        $this->price = $price;
+    }
+
+    public function setTime(string $time): void {
+        $this->time = $time;
+    }
+
+    public function setPicture(string $picture): void {
+        $this->picture = $picture;
+    }
+}
--- app/views/general_views/header.php
+++ app/views/general_views/header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Darumadrop+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Darumadrop+One&display=swap" rel="stylesheet">
     <title>HAARLEM FESTIVALS</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/navbar.css">
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/overview.css">
@@ -15,69 +17,76 @@
 
 
 </head>
-<body>
-<main>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="/../img/logo.png" alt="Logo" height="70">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0"> <!-- Changed here, from me-auto to ms-auto -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <img src="/../img/dutch.png" alt="NL" style="width: 20px;"> NL
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">Homepage</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">My Program</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Festivals
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="#">Music</a></li>
-                            <li><a class="dropdown-item" href="#">Yummy!</a></li>
-                            <li><a class="dropdown-item" href="/history/overview">Strolling Through History</a></li>
-                            <li><a class="dropdown-item" href="#">Art in the Open</a></li> <!-- Added fourth item -->
-                        </ul>
-                    </li>
-                </ul>
-                <?php
-                  if (isset($_SESSION['role'])) {
-                    $role = $_SESSION['role'];
-                   if ($role == 'customer') {
-                        echo ' <button class="btn btn-outline-success ms-2" type="button" onclick="location.href=\'/account\'">Account</button> ';
-                        echo ' <button class="btn btn-danger ms-2" type="button" onclick="confirmLogout()">Logout</button> ';
-                    } else {
-                        echo ' <button class="btn btn-outline-success ms-2" type="button" onclick="location.href=\'/login\'">Login</button> ';
-                        echo ' <button class="btn btn btn-dark ms-2" type="button" onclick="location.href=\'register\'">Register</button> ';
-                    }
-                } else {
-                    echo ' <button class="btn btn-outline-success ms-2" type="button" onclick="location.href=\'/login\'">Login</button> ';
-                    echo ' <button class="btn btn btn-dark ms-2" type="button" onclick="location.href=\'/register\'">Register</button> ';
-                }
-                ?>
-            </div>
-        </div>
-    </nav>
-    <div class="container">
-        <!-- Content goes here -->
-    </div>
-</main>
 
-<script type="text/javascript">
-    function confirmLogout() {
-        var logout = confirm("Are you sure you want to log out?");
-        if (logout) {
-            window.location.href = '/logout'; 
+<body>
+    <main>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="/../img/logo.png" alt="Logo" height="70">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0"> <!-- Changed here, from me-auto to ms-auto -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <img src="/../img/dutch.png" alt="NL" style="width: 20px;"> NL
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Homepage</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">My Program</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Festivals
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Music</a></li>
+                                <li><a class="dropdown-item" href="#">Yummy!</a></li>
+                                <li><a class="dropdown-item" href="/history/overview">Strolling Through History</a></li>
+                                <li><a class="dropdown-item" href="#">Art in the Open</a></li>
+                                <!-- Added fourth item -->
+                            </ul>
+                        </li>
+                    </ul>
+                    <?php
+                    if (isset($_SESSION['role'])) {
+                        switch ($_SESSION['role']) {
+                            case 'customer':
+                                echo '<button class="btn btn-outline-success ms-2" type="button" onclick="location.href=\'/account\'">Account</button>';
+                                echo '<button class="btn btn-danger ms-2" type="button" onclick="confirmLogout()">Logout</button>';
+                                break;
+                            default:
+                                echo '<button class="btn btn-outline-success ms-2" type="button" onclick="location.href=\'/login\'">Login</button>';
+                                echo '<button class="btn btn-dark ms-2" type="button" onclick="location.href=\'/register\'">Register</button>';
+                                break;
+                        }
+                    } else {
+                        echo '<button class="btn btn-outline-success ms-2" type="button" onclick="location.href=\'/login\'">Login</button>';
+                        echo '<button class="btn btn-dark ms-2" type="button" onclick="location.href=\'/register\'">Register</button>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <!-- Content goes here -->
+        </div>
+    </main>
+
+    <script type="text/javascript">
+        function confirmLogout() {
+            var logout = confirm("Are you sure you want to log out?");
+            if (logout) {
+                window.location.href = '/logout';
+            }
         }
-    }
-</script>
+    </script>
