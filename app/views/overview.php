22b18648ff21e89a237aa0b2f981dcefbb78090f
alfinaaura <659261@student.inholland.nl>
2024-02-28 00:37:49 +0100

    fixing navbar issues

--- app/views/overview.php
+++ app/views/overview.php
@@ -20,47 +20,47 @@ include __DIR__ . '/general_views/header.php';
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Grote Markt</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>De Hallen</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Proveniershof</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Jopenkerk</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Waalse Kerk Haarlem</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Molen de Adriaan</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Amsterdamse Poort</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Hof van Bakenes</p>
         </div>
     </div>
 </div>
 
-<div class="festival-banner"">
+<div class="festival-banner">
     <div class="festival-promotion-header" style="background-image: url(../img/dancePromotion.png);">
         <h2>Dance! the night away</h2>
     </div>
@@ -69,47 +69,175 @@ include __DIR__ . '/general_views/header.php';
         <!-- for every event / artist make this-->
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Nicki Romero</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Tiesto</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Hardwell</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Afrojack</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Armin Van Buuren</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Martin Garrix</p>
+        </div>
+    </div>
+</div>
+
+<div class="festival-banner">
+    <div class="festival-promotion-header" style="background-image: url(../img/jazzPromotion.png);">
+        <h2>Jazz! and chill</h2>
+    </div>
+    
+    <div class = "banner-content">
+        <!-- for every event / artist make this-->
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Gumbo Kings</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Evolve</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Ntjam Rose</p>
         </div>
 
         <div class="individual-event">
             <img src="/img/bavoKerkBanner.jpg" alt="">
-            <p>Church of st Bravo</p>
+            <p>Wicked jazz sounds</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Jonna Frazer</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Tom thomsom assemble</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Fox & The Mayors</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Uncle Sue</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Kris Allen</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Myles Sanko</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Soul Six</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Ruis Soundsystem</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>The Family XL</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Gare Du Nord</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Rilan and the bombardiers</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Han Bennink</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>The Nordanians</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Lilith Merlot</p>
+        </div>
+    </div>
+</div>
+
+<div class="festival-banner">
+    <div class="festival-promotion-header" style="background-image: url(../img/yummyPromotion.png);">
+        <h2>Yummy! Foodies </h2>
+    </div>
+    
+    <div class = "banner-content">
+        <!-- for every event / artist make this-->
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>cafe de roemer</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Ratatouille</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>ML Restaurant</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Fris Restaurant</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Specktacel</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Grand cafe Brinkman</p>
+        </div>
+
+        <div class="individual-event">
+            <img src="/img/bavoKerkBanner.jpg" alt="">
+            <p>Urban Frenchy Bistro Toujours</p>
         </div>
     </div>
 </div>
