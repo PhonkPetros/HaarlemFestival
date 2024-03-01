16b60a4b3f73431737647bc26ecac0d165084cf7
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-03-01 17:19:59 +0100

    getters and setters for all the new tables done

---
+++ app/model/section.php
@@ -0,0 +1,51 @@
+<?php
+namespace model;
+
+class Section implements \JsonSerializable
+{
+    private int $section_id;
+    private int $editor_id;
+    private int $image_id;
+    private int $page_id;
+
+    public function getSectionId(): int {
+        return $this->section_id;
+    }
+
+    public function setSectionId(int $section_id): void {
+        $this->section_id = $section_id;
+    }
+
+    public function getEditorId(): int {
+        return $this->editor_id;
+    }
+
+    public function setEditorId(int $editor_id): void {
+        $this->editor_id = $editor_id;
+    }
+
+    public function getImageId(): int {
+        return $this->image_id;
+    }
+
+    public function setImageId(int $image_id): void {
+        $this->image_id = $image_id;
+    }
+
+    public function getPageId(): int {
+        return $this->page_id;
+    }
+
+    public function setPageId(int $page_id): void {
+        $this->page_id = $page_id;
+    }
+
+    public function jsonSerialize(): mixed {
+        return [
+            'section_id' => $this->section_id,
+            'editor_id' => $this->editor_id,
+            'image_id' => $this->image_id,
+            'page_id' => $this->page_id,
+        ];
+    }
+}
