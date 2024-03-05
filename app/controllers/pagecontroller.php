18d422e91e6b1471ea625d1429b20c4492c82a5e
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-03-05 20:07:15 +0100

    minor changes

--- app/controllers/pagecontroller.php
+++ app/controllers/pagecontroller.php
@@ -4,32 +4,36 @@ namespace controllers;
 
 use services\pageservice;
 
-require_once __DIR__ ."/../services/pageservice.php";
+require_once __DIR__ . "/../services/pageservice.php";
 
 class Pagecontroller
 {
- private $pageService;
-  
-    public function __construct() {
+    private $pageService;
+    public function __construct()
+    {
         $this->pageService = new Pageservice();
     }
 
-    public function editContent(){
+    public function editContent()
+    {
         $allSections = $this->getSectionsFromPageID();
         $pageDetails = $this->getPageDetails();
-        require_once __DIR__ ."/../views/admin/page-managment/editPageOverview.php";
+        require_once __DIR__ . "/../views/admin/page-managment/editPageOverview.php";
     }
-    public function getSectionsFromPageID(){
-       $page = htmlspecialchars($_GET['id']);
-       $allSections = $this->pageService->getAllSections($page);
-       return $allSections;
+    public function getSectionsFromPageID()
+    {
+        $page = htmlspecialchars($_GET['id']);
+        $allSections = $this->pageService->getAllSections($page);
+        return $allSections;
     }
 
-    public function getPageDetails(){
+    public function getPageDetails()
+    {
         $page = htmlspecialchars($_GET['id']);
         $pageDetails = $this->pageService->getPageDetails($page);
         return $pageDetails;
-     }
+    }
+    
 
 
 }
