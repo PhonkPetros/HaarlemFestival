d3d9a8f5f43090d4ddcebd01f071dcadb0f56e75
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-03-16 08:33:14 +0100

    fixing routing

--- app/public/index.php
+++ app/public/index.php
@@ -35,8 +35,8 @@ require_once __DIR__ . '/../controllers/templatecontroller.php';
 require_once __DIR__ . '/../controllers/yummycontroller.php';
 require_once __DIR__ . '/../controllers/resetpasswordcontroller.php';
 
-$url = $_SERVER['REQUEST_URI'];
-$request = parse_url($url)['path'];
+
+$request = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
 
