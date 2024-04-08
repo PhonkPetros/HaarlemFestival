d25932af9105da21ee56ff824886dd848ec2f29f
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-04-08 17:26:57 +0200

    favicon added

--- app/views/general_views/adminheader.php
+++ app/views/general_views/adminheader.php
@@ -5,6 +5,13 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Darumadrop+One&display=swap" rel="stylesheet">
 <title><?php echo htmlspecialchars($pageTitle ?? "HAARLEM FESTIVALS"); ?></title>
+<link rel="apple-touch-icon" sizes="180x180" href="/../img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/../img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/../img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/../img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/../img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.tiny.cloud/1/dm3hxv1faylcg1xh3z1honx4wnt69zfinvo8edzoid1nltym/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
--- app/views/general_views/navbar.php
+++ app/views/general_views/navbar.php
@@ -6,6 +6,13 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Darumadrop+One&display=swap" rel="stylesheet">
     <title><?php echo htmlspecialchars($pageTitle ?? "HAARLEM FESTIVALS"); ?></title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/../img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/../img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/../img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/../img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/../img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/navbar.css">
